7f06e8722d376cc6bb64900db3d3b27caba9e92c
Claudia Chittim <cchittim10101@gmail.com>
2022-03-24 19:19:50 -0500

    practicing .foreach function

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-var commandLineArgs = process.argv;
-console.log(commandLineArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+printProfileData(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+  for (let i = 0; i < profileDataArr.length; i++) {
+    console.log(profileDataArr[i]);
+  }
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach((profileItem) => {
+    console.log(profileItem)
+  });
+};
+};
